8c3f1e4773097da8639cdc2e7fd62b4788e5f8e5
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-08 19:50:57 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import joplin from "api";
+import { MenuItemLocation } from "api/types";
+
+joplin.plugins.register({
+  onStart: async function () {
+    console.info("Combine plugin started");
+
+    await joplin.commands.register({
+      name: "CombineNotes",
+      label: "Combine selected notes",
+      execute: async () => {
+        const ids = await joplin.workspace.selectedNoteIds();
+        const newNoteBody = [];
+        let notebookId = null;
+
+        for (const noteId of ids) {
+          const note = await joplin.data.get(["notes", noteId], {
+            fields: ["title", "body", "parent_id"],
+          });
+          newNoteBody.push(["# " + note.title, "", note.body].join("\n"));
+
+          if (!notebookId) notebookId = note.parent_id;
+        }
+
+        const newNote = {
+          title: "Concatenated note",
+          body: newNoteBody.join("\n\n"),
+          parent_id: notebookId,
+        };
+
+        await joplin.data.post(["notes"], null, newNote);
+      },
+    });
+
+    await joplin.views.menuItems.create(
+      "myMenuItemToolsCombineNotes",
+      "CombineNotes",
+      MenuItemLocation.Tools
+    );
+    await joplin.views.menuItems.create(
+      'contextMenuItemconcatCombineNotes', 
+      'CombineNotes', 
+      MenuItemLocation.NoteListContextMenu
+    );
+  },
+});
